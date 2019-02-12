b6b9e376ed8102431b5200ec38c91d534a3ba79d
Richard Brunt <richard.brunt@tessella.com>
2019-02-12 09:56:01 +0000

    Fix flask-restful imports in custom dataset endpoint

--- app/resources/datasets.py
+++ app/resources/datasets.py
@@ -1,12 +1,12 @@
-from flask.ext.restful.inputs import boolean
-from flask.ext.restful.reqparse import Argument
+from flask_restful.inputs import boolean
+from flask_restful.reqparse import Argument
 from app.common import boilerplate
 
 
 from flask import current_app, request
-from flask.ext import restful
-from flask.ext.restful import abort, fields, marshal,marshal_with
-from flask.ext.restful import reqparse
+import flask_restful as restful
+from flask_restful import abort, fields, marshal,marshal_with
+from flask_restful import reqparse
 from app.common.auth import is_authenticated
 from app.common.rate_limit import rate_limit
 from app.common.response_templates import CTTVResponse
